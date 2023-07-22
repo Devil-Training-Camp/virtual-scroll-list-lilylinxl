f355b085110d0e791eaa27128afb95575336b3c5
linxiaoli <wb-lxl760186@alibaba-inc.com>
2023-07-22 14:07:23 +0800

    feat: 添加规范和自动化工具

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,24 +4,41 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:react-hooks/recommended",
+    "plugin:react/recommended",
+    "prettier"
   ],
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
     project: true,
-    tsconfigRootDir: __dirname,
+    tsconfigRootDir: __dirname
   },
-  plugins: ['react-refresh'],
+  plugins: [
+    "react",
+    "react-hooks",
+    "react-refresh",
+    "@typescript-eslint",
+    "prettier"
+  ],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true }
     ],
-    '@typescript-eslint/no-non-null-assertion': 'off',
-  },
-}
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "prettier/prettier": "error",
+    "no-undef": 2, //不可以有未声明的变量
+    "no-redeclare": 2, //禁止重复声明变量
+    semi: [2, "always"], //语句强制分号结尾
+    "no-func-assign": 2 //禁止重复的函数声明
+  }
+};
